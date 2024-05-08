972b594cb1be164aa9b214ccc4c3cbddba3c4ce4
JongD <JongD@JONGDO>
2024-05-08 13:04:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Assignment_13._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the rectangle:");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the width of the rectangle");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            double area= length * width;
+            Console.WriteLine("The area of a rectangle is: " + area);
+           
+            // his example
+
+        }   
+        static double AreaOfTriangle(double width, double height)
+        {
+           return (width * height) / 2;
+           
+        }
+        
+        static double AreaOfSquare(double side) 
+        {
+            return side*side;
+        }
+        
+        static double Area
+
+    }
+}
